9a6a84554ea504a57bb9c3ccc82ac901f08c7d9c
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-17 18:25:07 -0500

    Init

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    devServer: {
+        static: './dist'
+    },
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "index.[contenthash].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            ['@babel/preset-react', {runtime: 'automatic'}]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "public/index.html",
+            favicon: "public/favicon.png"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
