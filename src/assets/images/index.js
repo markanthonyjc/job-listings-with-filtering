d08da66454c84c3fa42650f643a24b7d4568b9f8
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-19 17:51:00 -0500

    Add main layout and global styles

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import BgHeaderDesktop from './bg-header-desktop.svg';
+import BgHeaderMobile from './bg-header-mobile.svg';
+import IconRemove from './icon-remove.svg';
+
+export {BgHeaderDesktop}
+export {BgHeaderMobile}
+export {IconRemove}
