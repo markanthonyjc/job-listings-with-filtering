94db4ccb646f1b335b656cc3e8f5113fe00bc1b3
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-09-24 13:01:54 -0500

    Add eslint and correct syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Header from "./components/layout/Header";
-import './assets/style.scss';
-import './App.scss';
+import Header from './components/layout/Header'
+import './assets/style.scss'
+import './App.scss'
 
 const App = () => {
-    return <>
+  return <>
         <Header></Header>
         <main></main>
-    </>;
-};
+    </>
+}
 
-export default App;
+export default App
