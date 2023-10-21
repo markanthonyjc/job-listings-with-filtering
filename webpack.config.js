24c23ebb525a5751dff182bdfd775d2deb04f5f1
Mark Anthony <44909345+markanthonyjc@users.noreply.github.com>
2023-10-21 18:56:37 -0500

    Add gh-pages package for publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-    devServer: {
-        static: './dist'
-    },
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "index.[contenthash].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(?:js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            ['@babel/preset-react', {runtime: 'automatic'}]
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true
-                        }
-                    },
-                    "sass-loader"
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
+  devServer: {
+    static: './dist'
+  },
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[contenthash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "public/index.html",
-            favicon: "public/favicon.png"
-        }),
-        new CleanWebpackPlugin()
-    ],
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      favicon: 'public/favicon.png'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 }
